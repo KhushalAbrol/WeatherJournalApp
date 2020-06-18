645914f431088b5c744de744bf246196e3e702e4
Khushal Abrol <64589440+KhushalAbrol@users.noreply.github.com>
2020-06-18 11:42:31 +0530

    Update app.js

--- Website/app.js
+++ Website/app.js
@@ -1,8 +1,8 @@
 const baseURL="http://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "&appid=e290b09f9a8816e3568a9e40b1fb9ef3&units=imperial";
 
-
 document.getElementById('generate').addEventListener('click',generateZip);
+
 
 function generateZip(){
     const zip=document.getElementById('zip').value;
@@ -10,38 +10,55 @@ function generateZip(){
     getWeatherInfo(url)
     //after data recived from API then store data(post data) in server
     .then(function(data){
-            console.log("hi Khushal");
-            postData('/add',{ temp: data.temp,
-                pressure:data.pressure,
-                humidity:data.humidity,
-                temp_min:data.temp_min,
-                temp_max:data.temp_max,
-                visibility:data.visibility,
-                timezone:data.timezone,
-                name:data.name       
+            //console.log("hi Khushal");
+            //console.log( data.name );
+            document.getElementById('zip').value="";
+            if(data.cod=="404"||data.message=="invalid zip code")
             
-            
-            
-            })  
-                console.log("postData")      ;
+            {   document.getElementById('value').style.color="red";
+                document.getElementById('temp').innerHTML ="Record Not Found!!!";
+                document.getElementById('pressure').innerHTML ="Record Not Found!!!";
+                document.getElementById('temp-min').innerHTML ="Record Not Found!!!";
+                document.getElementById('temp-max').innerHTML ="Record Not Found!!!";
+                document.getElementById('humidity').innerHTML ="Record Not Found!!!";
+                document.getElementById('visibility').innerHTML ="Record Not Found!!!";
+                document.getElementById('timezone').innerHTML ="Record Not Found!!!";
+                document.getElementById('name').innerHTML ="Record Not Found!!!"; 
+                //alert("City Not Found!!!");  
+        }
+            else{
+                document.getElementById('value').style.color="royalblue";
+                postData('/add',{   temp: data.main.temp,
+                                    pressure:data.main.pressure,
+                                    humidity:data.main.humidity,
+                                    temp_min:data.main.temp_min,
+                                    temp_max:data.main.temp_max,
+                                    visibility:data.visibility,
+                                    timezone:data.timezone,
+                                    name:data.name         
+            })
+//after the above task get completed then updateUI function executes:
+    .then (function(){
+        updateUI()
+    })}
     })
-    .then (updateUI())
+    
 };
 
 //Get weather information from weather api using url
 const getWeatherInfo = async (url) =>{
-
     const res = await fetch(url)
     try{
         const data = await res.json();
-        console.log(data);
+        //console.log(data);
         return data;
     }
     catch(error){
-        console.log("error"+error);
+        console.log("error",error);
     }
 }
 
+//postData function defination:
 const postData = async (url = '' ,data ={}) =>{
     //console.log(data)
     const response = await fetch(url, {
@@ -54,18 +71,20 @@ const postData = async (url = '' ,data ={}) =>{
     });
     try {
         const newData = await response.json();
-        console.log(newData);
+        //console.log(newData);
         return newData;
     }
       catch(error) {
-      console.log("error", error);
+        alert("error"+error);
       }
   }
 
+  //updateUI function defination:
 const updateUI = async() => {
-    const request = await fetch('/');
+    const request = await fetch('get');
     try{
         const allData = await request.json();
+        //console.log("updateUI working")
         document.getElementById('temp').innerHTML = allData[0].temp;
         document.getElementById('pressure').innerHTML = allData[0].pressure;
         document.getElementById('temp-min').innerHTML = allData[0].temp_min;
@@ -76,6 +95,8 @@ const updateUI = async() => {
         document.getElementById('name').innerHTML = allData[0].name;
     }
     catch(error){
-            console.log("error",error);
+            alert("error"+error);
     }
 }
+
+
